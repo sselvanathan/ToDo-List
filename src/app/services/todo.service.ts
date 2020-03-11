6b6c88d225f94a2e762d91dc28c1eb387a7ec076
sselvanathan <senthuranselvanathanbusiness@gmail.com>
2020-03-11 01:15:37 +0100

    add todoService component

---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() {
+  }
+
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: 'todo one',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'todo two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'todo three',
+        completed: false
+      },
+    ];
+  }
+}
